4267be7d813457044a6edb338476580e23b92edd
Paul Lorett Amazona <whatevergeek@gmail.com>
2019-03-13 12:58:01 +0800

    Figure is symmetric around the horizontal axis

--- fsharp_pbt/Diamond.fs
+++ fsharp_pbt/Diamond.fs
@@ -3,7 +3,9 @@ module Diamond
 open System
 
 let make letter = 
-    ['A' .. letter]
+    let letters = ['A' .. letter]
+    letters
+    @(letters |> List.rev |> List.tail)
     |> List.map string
     |> List.reduce(fun x y -> sprintf "%s%s%s" x Environment.NewLine y)
 
