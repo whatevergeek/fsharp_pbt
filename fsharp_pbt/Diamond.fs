600cc2d7b7a1990c21c760be204fc5cc5e373cf2
Paul Lorett Amazona <whatevergeek@gmail.com>
2019-03-14 14:33:39 +0800

    Refactored solution: Removed 'A' special case, Introduced mirrorAndFuse helper function, Renamed letters to indexedLetters

--- fsharp_pbt/Diamond.fs
+++ fsharp_pbt/Diamond.fs
@@ -3,25 +3,22 @@ module Diamond
 open System
 
 let make letter = 
+    let mirrorAndFuse l = l @ (l |> List.rev |> List.tail)
     let makeLine letterCount (letter, letterIndex) = 
         let leadingSpace = String(' ', letterCount - 1 - letterIndex)
         let innerSpace = String(' ', letterCount - 1 - leadingSpace.Length)
-        match letter with
-        | 'A' ->        
-            sprintf "%s%c%s" leadingSpace letter leadingSpace
-        | _ ->
-            let left = 
-                sprintf "%s%c%s" leadingSpace letter innerSpace
-                |> Seq.toList
-            left
-            @(left |> List.rev |> List.tail)
-            |> List.map string
-            |> List.reduce (sprintf "%s%s")
+        let left = 
+            sprintf "%s%c%s" leadingSpace letter innerSpace
+            |> Seq.toList
+        left
+        |> mirrorAndFuse
+        |> List.map string
+        |> List.reduce (sprintf "%s%s")
 
-    let letters = ['A' .. letter] |> List.mapi (fun i l -> l, i)
-    letters
-    @(letters |> List.rev |> List.tail)
-    |> List.map (makeLine letters.Length)
+    let indexedLetters = ['A' .. letter] |> List.mapi (fun i l -> l, i)
+    indexedLetters
+    |> mirrorAndFuse
+    |> List.map (makeLine indexedLetters.Length)
     |> List.reduce(fun x y -> sprintf "%s%s%s" x Environment.NewLine y)
 
 
